2932bc868b269fee3e26cf78bac5deebc91ae592
TANJIANQUAN <tan472146@gmail.com>
2024-09-01 20:06:25 +0800

    logo

--- src/Boundary/CharityUI.java
+++ src/Boundary/CharityUI.java
@@ -11,10 +11,10 @@ package Boundary;
 import java.util.Scanner;
 
 public class CharityUI {
-    
+
     private Scanner scanner = new Scanner(System.in);
-    
-    public String getMenu(){
+
+    public String getMenu() {
         System.out.println(""
                 + "\nMAIN MENU"
                 + "\n 1. Donor Management"
@@ -26,8 +26,8 @@ public class CharityUI {
         String opt = scanner.nextLine();
         return opt;
     }
-    
-    public void getLogo(){
+
+    public void getLogo() {
         System.out.println(" _______         __        __      __   ___        __    __         __       ___________    __           ______     __    __         __        _______     __      ___________   ___  ___  ");
         System.out.println("|   _  \"\\       /\"\"\\      |\" \\    |/\"| /  \")      /\" |  | \"\\       /\"\"\\     (\"     _   \")  |\" \\         /\" _  \"\\   /\" |  | \"\\       /\"\"\\      /\"      \\   |\" \\    (\"     _   \") |\"  \\/\"  | ");
         System.out.println("(. |_)  :)     /    \\     ||  |   (: |/   /      (:  (__)  :)     /    \\     )__/  \\\\__/   ||  |       (: ( \\___) (:  (__)  :)     /    \\    |:        |  ||  |    )__/  \\\\__/   \\   \\  /  ");
@@ -35,6 +35,6 @@ public class CharityUI {
         System.out.println("(|  _  \\\\    //  __'  \\   |.  |   (// _  \\        //  __  \\\\    //  __'  \\      |.  |      |.  |        //  \\ _    //  __  \\\\    //  __'  \\   //      /   |.  |       |.  |       /   /    ");
         System.out.println("|: |_)  :)  /   /  \\\\  \\  /\\  |\\  |: | \\  \\      (:  (  )  :)  /   /  \\\\  \\     \\:  |      /\\  |\\      (:   _) \\  (:  (  )  :)  /   /  \\\\  \\ |:  __   \\   /\\  |\\      \\:  |      /   /     ");
         System.out.println("(_______/  (___/    \\___)(__\\_|_) (__|  \\__)      \\__|  |__/  (___/    \\___)     \\__|     (__\\_|_)      \\_______)  \\__|  |__/  (___/    \\___)|__|  \\___) (__\\_|_)      \\__|     |___/     ");
-                
+
     }
 }
